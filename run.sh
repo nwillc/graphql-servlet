8f46245c40ba3f89d5fd893ce1f09e0f160952b9
Nicholas Christopher <nwillc@gmail.com>
2017-02-14 23:20:31 -0500

    Genesis

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd -P)"
+
+cd ${SCRIPT_DIR}
+
+[ -f env.sh ] && source env.sh
+
+echo Rebuild server...
+./gradlew -q clean oneJar -x test
+[ $? != 0 ] && exit 1
+JAVA_OPTS="-Djava.awt.headless=true -Xmx20M"
+
+echo Start server...
+java ${JAVA_OPTS} -jar build/libs/*-standalone.jar $*
+
