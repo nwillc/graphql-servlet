ca4cd8bf1b6e145e9bfa0f6c77c630b74f5aeed3
Christopher, Nicholas (CORP) <Nicholas.Christopher@adp.com>
2017-02-15 14:01:48 -0500

    CLI support

--- src/main/java/com/github/nwillc/undertow/ServletServer.java
+++ src/main/java/com/github/nwillc/undertow/ServletServer.java
@@ -1,13 +1,18 @@
 package com.github.nwillc.undertow;
 
+import com.github.nwillc.undertow.CliOptions.CLI;
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 import javax.servlet.ServletException;
+
+import java.io.IOException;
 
 import static io.undertow.servlet.Servlets.*;
 
@@ -15,22 +20,34 @@ public class ServletServer {
 
     private final static String APP = "/graphql";
 
-    public static void main(final String[] args) throws ServletException {
-        DeploymentInfo servletBuilder = deployment()
+    public static void main(final String[] args) throws ServletException, IOException {
+        final OptionParser parser = CliOptions.getOptions();
+
+        final OptionSet options = parser.parse(args);
+
+        if (options.has(CLI.help.name())) {
+            parser.printHelpOn(System.out);
+            System.exit(0);
+        }
+
+        Integer port = (Integer) options.valueOf(CLI.port.name());
+        String address = (String) options.valueOf(CLI.address.name());
+
+        final DeploymentInfo servletBuilder = deployment()
                 .setClassLoader(ServletServer.class.getClassLoader())
                 .setContextPath(APP)
                 .setDeploymentName(ServletServer.class.getSimpleName() + ".war")
                 .addServlets(
                         servlet(GraphQLServlet.class.getSimpleName(), GraphQLServlet.class)
                                 .addMapping("/*"));
-        DeploymentManager manager = defaultContainer().addDeployment(servletBuilder);
+        final DeploymentManager manager = defaultContainer().addDeployment(servletBuilder);
         manager.deploy();
 
-        HttpHandler servletHandler = manager.start();
-        PathHandler path = Handlers.path(Handlers.redirect(APP))
+        final HttpHandler servletHandler = manager.start();
+        final PathHandler path = Handlers.path(Handlers.redirect(APP))
                 .addPrefixPath(APP, servletHandler);
-        Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
+        final Undertow server = Undertow.builder()
+                .addHttpListener(port, address)
                 .setHandler(path)
                 .build();
         server.start();
