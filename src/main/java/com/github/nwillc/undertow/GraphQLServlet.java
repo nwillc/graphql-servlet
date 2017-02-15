8119ab532863d88fd6f7197558c0aff9058af003
Nicholas Christopher <nwillc@gmail.com>
2017-02-15 01:45:27 -0500

    Fleshing out GraphGL POST

--- src/main/java/com/github/nwillc/undertow/GraphQLServlet.java
+++ src/main/java/com/github/nwillc/undertow/GraphQLServlet.java
@@ -1,20 +1,47 @@
 package com.github.nwillc.undertow;
 
 
+import com.github.nwillc.undertow.util.JsonMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class GraphQLServlet extends HttpServlet {
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.util.stream.Collectors.joining;
 
+public class GraphQLServlet extends HttpServlet implements JsonMapper {
+    static final String GRAPHQL_PATH = "graphql";
+    private static final String QUERY = "query";
+    private static final String ERRORS = "errors";
+    private static final String DATA = "data";
+    private static final String VARIABLES = "variables";
+
+    @SuppressWarnings("unchecked")
     @Override
-    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write("Hello World 2");
-        writer.close();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String body = req.getReader().lines().collect(joining("\n"));
+        Map<String, Object> payload;
+        try {
+            payload = getMapper().readValue(body, Map.class);
+        } catch (IOException e) {
+            resp.sendError(HTTP_BAD_REQUEST, "Could not parse request body as GraphQL map.");
+            return;
+        }
+        Map<String, Object> variables = (Map<String, Object>) payload.get("variables");
+
+        Map<String, Object> result = new LinkedHashMap<>();
+        result.put(DATA, payload);
+
+        resp.setContentType("application/json");
+        try (PrintWriter writer = resp.getWriter()) {
+            getMapper().writeValue(writer, result);
+        }
     }
-    
+
 }
